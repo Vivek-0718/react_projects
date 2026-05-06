718255647c3bb32c1356d6e2e4f35670d26cb909
Vivek <mail2viveksethuraman@gmail.com>
2026-05-06 16:18:46 +0530

    UMDb - tab focus enabled

--- umdb/src/components/Nav.js
+++ umdb/src/components/Nav.js
@@ -1,5 +1,21 @@
+import { useEffect, useRef } from "react";
 
 export default function Nav({ children, searchValue, handleSearch }) {
+  const inputElement = useRef(null)
+  useEffect(function () {
+    inputElement.current.focus()
+  }, [])
+  useEffect(function () {
+    function callback(e) {
+      if (e.key === "Enter") {
+        inputElement.current.focus();
+      }
+    }
+    document.addEventListener("keydown", callback);
+    return () => {
+      return document.removeEventListener("keydown", callback);
+    }
+  },[])
   return (
     <nav className="nav-bar">
       <div className="logo">
@@ -11,6 +27,7 @@ export default function Nav({ children, searchValue, handleSearch }) {
         placeholder="Search movies..."
         value={searchValue}
         onChange={(e) => handleSearch(e.target.value)}
+        ref={inputElement}
       />
       <p className="num-results">{children} results</p>
     </nav>
