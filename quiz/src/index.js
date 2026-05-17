be5fb34d59bf9f811f50ff4dfaad8849ca0ab3f6
Vivek <mail2viveksethuraman@gmail.com>
2026-05-17 14:11:17 +0530

    Quiz - prod error fix

--- quiz/src/index.js
+++ quiz/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { QuizProvider, useData } from "./context/quizprovider";
+import { QuizProvider } from "./context/quizprovider";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
