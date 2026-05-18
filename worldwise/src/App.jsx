ab06e36246db041796a9530514d0dc62f1c00914
Vivek <mail2viveksethuraman@gmail.com>
2026-05-18 13:54:19 +0530

    worldwise - suspence loader

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,31 +1,35 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage";
-import AppLayout from "./pages/AppLayout";
-import PageNotFound from "./pages/PageNotFound";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import SpinnerFullPage from "./components/SpinnerFullPage";
 import { ProvideContext } from "./context/Contextprovider";
+const Homepage = lazy(() => import("./pages/Homepage"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 function App() {
   return (
     <>
       <ProvideContext>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage></Homepage>}></Route>
-            <Route path="/app" element={<AppLayout />}>
-              <Route index element={<Navigate replace to="city" />}></Route>
-              <Route path="city" element={<CityList></CityList>}></Route>
-              <Route path="city/:id" element={<City></City>}></Route>
-              <Route
-                path="country"
-                element={<CountryList></CountryList>}
-              ></Route>
-              <Route path="form" element={<Form />}></Route>
-            </Route>
-            <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
-          </Routes>
+          <Suspense fallback={<SpinnerFullPage></SpinnerFullPage>}>
+            <Routes>
+              <Route index element={<Homepage></Homepage>}></Route>
+              <Route path="/app" element={<AppLayout />}>
+                <Route index element={<Navigate replace to="city" />}></Route>
+                <Route path="city" element={<CityList></CityList>}></Route>
+                <Route path="city/:id" element={<City></City>}></Route>
+                <Route
+                  path="country"
+                  element={<CountryList></CountryList>}
+                ></Route>
+                <Route path="form" element={<Form />}></Route>
+              </Route>
+              <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </ProvideContext>
     </>
--- worldwise/src/pages/PageNotFound.jsx
+++ worldwise/src/pages/PageNotFound.jsx
@@ -1,7 +1,14 @@
 export default function PageNotFound() {
   return (
-    <div>
-      <h1>Page not found 😢</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height:"100%"
+      }}
+    >
+      <h1 style={{ color: "#000" }}>Page not found 😢</h1>
     </div>
   );
 }
